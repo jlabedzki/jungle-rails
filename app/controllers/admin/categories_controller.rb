c05403a16ac0e4af5d8160663d7bd71ecebc9000
Johnny <johnnylabedzki@hotmail.com>
2021-10-13 15:41:08 -0700

    Create route for admin/categories and create view files for index, new, and create

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,18 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+end
