ea8dc59cb8bcf50438353001e6752fa35a1d41ac
Johnny <johnnylabedzki@hotmail.com>
2021-10-18 10:03:26 -0700

    implement tests for validation, email uniqueness, password length, and password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, confirmation: { case_sensitive: true }
+  validates :password_confirmation, presence: true
+
+  def authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email)
+
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,125 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+
+    it "saves a user when all fields are filled correctly" do
+      @user_count = User.all.count
+
+      @user = User.new(
+        name: "Joe Bloe",
+        email: "joe@bloe.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      @user.save!
+
+      expect(User.all.count).to eq(@user_count + 1)
+    end
+
+    it "should give an error if no name is given" do
+      @user = User.new(
+        name: nil,
+        email: "joe@bloe.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+
+    end
+
+    it "should give an error if no email is given" do
+      @user = User.new(
+        name: "Joe Bloe",
+        email: nil,
+        password: "password", 
+        password_confirmation: "password"
+      )
+
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "should give an error if no password is given" do
+      @user = User.new(
+        name: "Joe Bloe",
+        email: "joe@bloe.ca",
+        password: nil, 
+        password_confirmation: "password"
+      )
+
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "should give an error if no password confirmation is given" do
+      @user = User.new(
+        name: "Joe Bloe",
+        email: "joe@bloe.ca",
+        password: "password", 
+        password_confirmation: nil
+      )
+
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
+    it "should give an error if email is not unique (not case sensitive)" do
+      @user = User.new(
+        name: "Joe Bloe",
+        email: "JOE@BLOE.CA",
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      @user.save!
+
+      @user2 = User.new(
+        name: "Jane Doe",
+        email: "joe@bloe.ca",
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      @user2.validate
+
+      expect(@user2.errors.full_messages).to include('Email has already been taken')
+    end
+
+    it "should give an error if password and password confirmation do not match (case sensitive)" do
+      @user = User.new(
+        name: "Joe Bloe",
+        email: "joe@bloe.ca",
+        password: "PASSWORD",
+        password_confirmation: "password"
+      )
+
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "should give an error if password is less than 8 characters long" do
+      @user = User.new(
+        name: "Joe Bloe",
+        email: "joe@bloe.ca",
+        password: "pass",
+        password_confirmation: "pass"
+      )
+      
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+    end
+
+  end
+
+  describe ".authenticate_with_credentials" do
+  end
+end
